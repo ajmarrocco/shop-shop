0f392dddcdf41bd9f5b58bd178e84a351ecfb696
ajmarrocco <ajmarrocco@gmail.com>
2022-05-06 21:51:32 -0400

    edit components to take global state variables

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from "../../utils/GlobalState";
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+  // retrieves the current state from global variable, and dispatch to update state
+  const [state, dispatch] = useStoreContext();
+
+  // destructures categories out of state
+  const { categories } = state;
+  
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  // Retrieves category from server and saves it to global state object and uses data to print list of categories
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map((item) => (
+      {categories.map(item => (
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
