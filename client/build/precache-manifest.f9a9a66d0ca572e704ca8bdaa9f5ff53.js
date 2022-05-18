6f5cd7f21b420c590698704fa68beb5d9c64bcd8
ajmarrocco <ajmarrocco@gmail.com>
2022-05-18 15:53:50 -0400

    add build folder

---
+++ client/build/precache-manifest.f9a9a66d0ca572e704ca8bdaa9f5ff53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e582a0fe380047a409edc71a7c75cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df7ca7a36948b849ab8e",
+    "url": "/static/css/main.b2a0044d.chunk.css"
+  },
+  {
+    "revision": "76da5b715fbb9a53bd9a",
+    "url": "/static/js/2.f117b72c.chunk.js"
+  },
+  {
+    "revision": "cf1756f17a9fe3b6b59b94401b81c70b",
+    "url": "/static/js/2.f117b72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7ca7a36948b849ab8e",
+    "url": "/static/js/main.62387ca8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
